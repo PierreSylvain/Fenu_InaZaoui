848ef97aa7789c9bb67e3fedab71b23a64547496
Laurent Fenu <laurentfenu.devweb@gmail.com>
2024-11-26 11:52:34 +0100

    create SecutityControllerTest.php

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private EntityManagerInterface $entityManager;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->entityManager = self::getContainer()->get(EntityManagerInterface::class);
+    }
+
+    public function testLoginPage(): void
+    {
+        $this->client->request('GET', '/login');
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h1', 'Connexion');
+    }
+}
