5ffd8f69878169abe550ee6d2118155f309a7a87
Laurent Fenu <laurentfenu.devweb@gmail.com>
2024-11-26 11:44:42 +0100

    update SecurityController.php

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -3,23 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'admin_login')]
-    public function login(AuthenticationUtils $authenticationUtils, AuthorizationCheckerInterface $authorizationChecker): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // obtenir l'erreur de connexion s'il y en a une
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // dernier nom d'utilisateur saisi
-        $lastUsername = $authenticationUtils->getLastUsername();
-
         $user = $this->getUser();
 
         // if ($user === null) {
@@ -27,13 +20,16 @@ class SecurityController extends AbstractController
         //     return $this->redirectToRoute('home');
         // }
 
-        // dd($user);
-        if ($user instanceof User && $user->isRestricted() === true) {
-        // if ($user instanceof User && !in_array('ROLE_USER_RESTRICTED', $user->getRoles())) {
-        // if ($authorizationChecker->isGranted('ROLE_USER_RESTRICTED') && !$this->getUser()) {
+        // obtenir l'erreur de connexion s'il y en a une
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // dernier nom d'utilisateur saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        if ($user instanceof User && $user->isRestricted()) {
             $this->addFlash('error', 'Accès refusé.');
             return $this->redirectToRoute('home');
         }   
+        
         return $this->render('admin/login.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
